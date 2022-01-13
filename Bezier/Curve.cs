13afcb445d24de88efc8729887de420358a437dc
KibbeWater <92534409+LinusNTI@users.noreply.github.com>
2022-01-13 06:58:31 +0100

    Initial Commit

---
+++ Bezier/Curve.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Bezier
+{
+    public struct Vector2D {
+        public Vector2D(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Vector2D zero = new Vector2D(0, 0);
+
+        public double x { get; }
+        public double y { get; }
+
+        public static Vector2D operator -(Vector2D a, Vector2D b) => new Vector2D(a.x - b.x, a.y - b.y);
+        public static Vector2D operator +(Vector2D a, Vector2D b) => new Vector2D(a.x + b.x, a.y + b.y);
+        public static Vector2D operator *(Vector2D a, Vector2D b) => new Vector2D(a.x * b.x, a.y * b.y);
+
+        public static Vector2D operator *(Vector2D a, double b) => new Vector2D(a.x * b, a.y * b);
+
+        public static bool operator ==(Vector2D a, Vector2D b) => a.x == b.x && a.y == b.y;
+        public static bool operator !=(Vector2D a, Vector2D b) => a.x != b.x || a.y != b.y;
+
+        public static implicit operator PointF(Vector2D d) => new PointF((float)d.x, (float)d.y);
+
+        public override string ToString() => $"({x}, {y})";
+    }
+
+    public struct VectorPair {
+        public Vector2D p1, p2;
+
+        public VectorPair(Vector2D p1, Vector2D p2) {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+    }
+
+    public class Curve {
+        public Vector2D p1, p2;
+        public List<Vector2D> points = new List<Vector2D>();
+
+        private Vector2D Interpolate(Vector2D p1, Vector2D p2, double t) => p1 + (p2 - p1) * t;
+
+        public void AddPoint(Vector2D p) => points.Add(p);
+
+        public Vector2D GetPoint(double t)
+        {
+            if (points.Count < 1) throw new InvalidPointsException();
+
+            List<Vector2D> p = new List<Vector2D>();
+            p.Add(p1);
+            for (int i = 0; i < points.Count; i++)
+                p.Add(points[(points.Count - 1) - i]);
+            p.Add(p2);
+
+            List<VectorPair> lines = new List<VectorPair>();
+            for (int i = 1; i < p.Count; i++) {
+                var curPoint = p[i];
+                var oldPoint = p[i - 1];
+                lines.Add(new VectorPair(oldPoint, curPoint));
+            }
+
+            return ComputeLines(lines, t);
+        }
+
+        private Vector2D ComputeLines(List<VectorPair> lines, double t) {
+            if (lines.Count > 2) {
+                List<VectorPair> newLines = new List<VectorPair>();
+                for (int i = 1; i < lines.Count; i++) {
+                    var curLine = lines[i];
+                    var oldLine = lines[i - 1];
+                    newLines.Add(new VectorPair(Interpolate(oldLine.p1, oldLine.p2, t), Interpolate(curLine.p1, curLine.p2, t)));
+                }
+                return ComputeLines(newLines, t);
+            } else {
+                var point1Diff = Interpolate(lines[0].p1, lines[0].p2, t);
+                var point2Diff = Interpolate(lines[1].p1, lines[1].p2, t);
+                return point1Diff + (point2Diff - point1Diff) * t;
+            }
+        }
+    }
+
+
+    [Serializable]
+    public class InvalidPointsException : Exception
+    {
+        public InvalidPointsException() { }
+        public InvalidPointsException(string message) : base(message) { }
+        public InvalidPointsException(string message, Exception inner) : base(message, inner) { }
+        protected InvalidPointsException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ Bezier/Main.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Bezier
+{
+    public partial class Main : Form
+    {
+        private System.Timers.Timer _updateTimer = new System.Timers.Timer(1000/144);
+        private Curve _bezier = new Curve();
+        private Random r = new Random();
+
+        private List<Pen> lineClrs = new List<Pen>();
+        private bool computeLines = true;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private new void KeyDown(object sender, PreviewKeyDownEventArgs e) {
+            if (e.KeyCode == Keys.Z) // Reset bezier points
+                _bezier = new Curve();
+            if (e.KeyCode == Keys.X) // Toggle animated lines
+                computeLines = !computeLines;
+        }
+
+        private new void Click(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Right)
+                _bezier.p1 = new Vector2D(e.X, e.Y);
+            if (e.Button == MouseButtons.Left)
+                _bezier.p2 = new Vector2D(e.X, e.Y);
+            if (e.Button == MouseButtons.Middle)
+                _bezier.AddPoint(new Vector2D(e.X, e.Y));
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            _updateTimer.Elapsed += Update;
+            _updateTimer.Start();
+
+            // Generate colors for animation
+            for (int i = 0; i < 100; i++) {
+                lineClrs.Add(new Pen(Color.FromArgb(r.Next(0,255),r.Next(0,255),r.Next(0,255))));
+            }
+        }
+
+        private void Update(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            // Update functions, runs every frame
+
+            // Invalidate form, force re-rendering and force Draw to run
+            this.Invalidate();
+        }
+
+        private void Draw(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            #region Draw point path
+            List<Vector2D> points = new List<Vector2D>();
+            points.Add(_bezier.p1);
+            for (int i = 0; i < _bezier.points.Count; i++)
+                points.Add(_bezier.points[(_bezier.points.Count-1) - i]);
+            points.Add(_bezier.p2);
+
+            for (int i = 0; i < points.Count; i++) {
+                var point = points[i];
+                g.DrawEllipse(Pens.Blue, (float)point.x-5, (float)point.y-5, 10, 10);
+
+                if (i == 0) continue;
+
+                var oldPoint = points[i - 1];
+                g.DrawLine(Pens.Red, oldPoint, point);
+            }
+            #endregion
+
+            #region Draw vertical alignment line
+            var definedPoint = (_bezier.p1 != Vector2D.zero) ? _bezier.p1 : (_bezier.p2 != Vector2D.zero) ? _bezier.p2 : Vector2D.zero;
+            var cursorPos = this.PointToClient(Cursor.Position);
+
+            if (Math.Abs(cursorPos.Y - definedPoint.y) < 15)
+                g.DrawLine(Pens.Black, 0, (float)definedPoint.y, this.Width, (float)definedPoint.y);
+            #endregion
+
+            #region Draw height line
+            if (_bezier.p1 != Vector2D.zero && _bezier.p2 != Vector2D.zero)
+                if (cursorPos.X - _bezier.p1.x <= 0 && cursorPos.X - _bezier.p2.x >= 0) {
+                    var bezierLength = _bezier.p2.x - _bezier.p1.x;
+                    var cursorRel = cursorPos.X - _bezier.p1.x;
+
+                    var t = ((100 / bezierLength) * cursorRel) / 100;
+
+                    var point = _bezier.p1 + (_bezier.p2 - _bezier.p1)*t;
+
+                    g.DrawEllipse(Pens.Black, (float)point.x - 5, (float)point.y - 5, 10, 10);
+                    g.DrawEllipse(Pens.Black, (float)cursorPos.X - 5, (float)cursorPos.Y - 5, 10, 10);
+                    g.DrawLine(Pens.Black, point, cursorPos);
+                    g.DrawString("Height: " + Math.Abs(Math.Floor(point.y - cursorPos.Y)), DefaultFont, Brushes.Black, new PointF(cursorPos.X, (float)(point.y + (cursorPos.Y - point.y)/2f)));
+                }
+            #endregion
+
+            #region Draw Bezier
+            if (_bezier.points.Count < 1) return;
+
+            var path = new GraphicsPath();
+            var resolution = 100f;
+            for (int i = 1; i < resolution; i++) {
+                var t = i / resolution;
+                var tOld = (i - 1) / resolution;
+
+                var curPoint = _bezier.GetPoint(t);
+                var oldPoint = _bezier.GetPoint(tOld);
+                path.AddLine(curPoint, oldPoint);
+            }
+            g.DrawPath(Pens.Black, path);
+            #endregion
+
+            #region Animate
+            float animationTime = 2f;
+            float time = ((animationTime - (Environment.TickCount / 1000f) % animationTime)) / animationTime;
+
+            var p = _bezier.GetPoint(time);
+
+            List<VectorPair> lines = new List<VectorPair>();
+            for (int i = 1; i < points.Count; i++) {
+                var curPoint = points[i];
+                var oldPoint = points[i - 1];
+                lines.Add(new VectorPair(oldPoint, curPoint));
+            }
+            if (computeLines)
+                DrawComputedLines(lines, time, g);
+            g.DrawEllipse(Pens.Blue, (float)p.x - 5, (float)p.y - 5, 10, 10);
+
+            g.DrawEllipse(Pens.Black, (float)p.x - 5, 10, 10, 10);
+            g.DrawEllipse(Pens.Black, (float)_bezier.p1.x-5, 10, 10, 10);
+            g.DrawEllipse(Pens.Black, (float)_bezier.p2.x-5, 10, 10, 10);
+            g.DrawLine(Pens.Black, (float)_bezier.p1.x-5, 15, (float)_bezier.p2.x+5, 15);
+            #endregion
+        }
+
+        #region Animation methods
+        // Copy-pasted from the Curve class, only for animation
+        private void DrawComputedLines(List<VectorPair> lines, double t, Graphics g, int depth = 0) {
+            if (lines.Count > 2) {
+                List<VectorPair> newLines = new List<VectorPair>();
+                for (int i = 1; i < lines.Count; i++) {
+                    var curLine = lines[i];
+                    var oldLine = lines[i - 1];
+                    var newLine = new VectorPair(Interpolate(oldLine.p1, oldLine.p2, t), Interpolate(curLine.p1, curLine.p2, t));
+                    g.DrawLine(lineClrs[depth], newLine.p1, newLine.p2);
+                    newLines.Add(newLine);
+                }
+                DrawComputedLines(newLines, t, g, depth+1);
+            } else {
+                var point1Diff = Interpolate(lines[0].p1, lines[0].p2, t);
+                var point2Diff = Interpolate(lines[1].p1, lines[1].p2, t);
+                g.DrawLine(lineClrs[depth], point1Diff, point2Diff);
+            }
+        }
+
+        private Vector2D Interpolate(Vector2D p1, Vector2D p2, double t) => p1 + (p2 - p1) * t;
+        #endregion
+    }
+}
